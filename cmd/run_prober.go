e666f8c94ff7c0567794660b960c6edc48116ee9
hossainemruz <hossainemruz@gmail.com>
2019-11-01 19:05:19 +0600

    WIP: Add demo exec probe

--- cmd/run_prober.go
+++ cmd/run_prober.go
@@ -2,18 +2,30 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 	"github.com/tamalsaha/prober-demo/probe"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"log"
 )
 
-func NewCmdRunProbe() *cobra.Command  {
+func NewCmdRunProbe() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "run-probe",
 		Short: "run probe",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			fmt.Println("Running... probe")
-			return probe.RunProbes()
-		} ,
+			kubeconfigPath := os.Getenv("KUBECONFIG")
+			config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+			if err != nil {
+				log.Fatalf("Could not get Kubernetes config: %s", err)
+			}
+			kubeClient := kubernetes.NewForConfigOrDie(config)
+			return probe.RunProbes(kubeClient)
+		},
 	}
 	return cmd
 }
